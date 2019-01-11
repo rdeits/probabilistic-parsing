f31eff1135ef8332f17c542b1f3bfc24db949bd7
Robin Deits <robin.deits@gmail.com>
2019-01-10 23:58:49 -0500

    faster similarity

--- SemanticSimilarity/src/SemanticSimilarity.jl
+++ SemanticSimilarity/src/SemanticSimilarity.jl
@@ -4,64 +4,59 @@ using WordNet
 using ProgressMeter
 
 const db = Ref{WordNet.DB}()
-
-const synsets = Dict{String, Vector{SynsetVertex}}()
+const Path = Vector{Synset}
+const paths = Dict{String, Vector{Path}}()
 
 function wu_palmer_similarity(w1::AbstractString, w2::AbstractString)
-    if !(w1 in keys(synsets)) || !(w2 in keys(synsets))
+    if !(w1 in keys(paths)) || !(w2 in keys(paths))
         0.0
     else
-        maximum(wu_palmer_similarity(db[], s1, s2) for s1 in synsets[w1], s2 in synsets[w2])::Float64
+        s = 0.0
+        for p1 in paths[w1]
+            for p2 in paths[w2]
+                s = max(s, wu_palmer_similarity(p1, p2))
+            end
+        end
+        s
     end
 end
 
-function wu_palmer_similarity(db::DB, v1::SynsetVertex, v2::SynsetVertex)
-    ancestor_depth = common_ancestor_depth(db, v1, v2)
-    2 * ancestor_depth / (v1.depth + v2.depth)
+function wu_palmer_similarity(p1::Path, p2::Path)
+    2 * common_ancestor_depth(p1, p2) / (length(p1) + length(p2))
 end
 
-function common_ancestor_depth(db::DB, v1::SynsetVertex, v2::SynsetVertex)
-    s1 = v1.synset
-    d1 = v1.depth
-    s2 = v2.synset
-    d2 = v2.depth
-    while true
-        if (d1 == 1 || d2 == 1) || (d1 == d2 && s1 == s2)
-            return d1
-        end
-        if d1 >= d2
-            s1 = hypernyms(db, s1)
-            d1 -= 1
-        end
-        if d2 > d1
-            s2 = hypernyms(db, s2)
-            d2 -= 1
+function common_ancestor_depth(p1::Path, p2::Path)
+    max_depth = min(length(p1), length(p2))
+    for i in 1:max_depth
+        if p1[i] != p2[i]
+            return i - 1
         end
     end
+    return max_depth
 end
 
-function depth(db::DB, synset::Synset)
-    depth = 1
+function path_to_synset(db::DB, synset::Synset)
+    result = [synset]
     while synset != WordNet.∅
         synset = hypernyms(db, synset)
-        depth += 1
-        if depth > 100
-            # @info "Possible cycle detected involving synset: $synset"
+        push!(result, synset)
+        if length(result) > 50
             return nothing
         end
     end
-    depth
+    reverse!(result)
+    result
 end
 
 function cache_synsets!()
-    empty!(synsets)
+    empty!(paths)
     for (pos, part_of_speech_synsets) in db[].synsets
         for synset in values(part_of_speech_synsets)
-            d = depth(db[], synset)
-            if d !== nothing
+            path = path_to_synset(db[], synset)
+            if path !== nothing
                 for word in words(synset)
-                    v = get!(Vector{SynsetVertex}, synsets, word)
-                    push!(v, SynsetVertex(synset, d))
+                    v = get!(Vector{Path}, paths, word)
+                    push!(v, path)
                 end
             end
         end
